9c957cc6cb767494eed7994884898dfe53d0ef3e
Tao707499 <120688956+Tao707499@users.noreply.github.com>
2023-02-23 23:02:35 -0500

    Update LF0_temp.py
    Extracts the 'resolvedValues' of locations and categories from chatbot's response.

--- Lambda/LF0_temp.py
+++ Lambda/LF0_temp.py
@@ -4,7 +4,14 @@ import json
 # Define the client to interact with Lex
 client = boto3.client('lexv2-runtime')
 
-def lambda_handler(event, context):    
+def lambda_handler(event, context):
+
+    # msg_from_user = event['messages'][0]["unstructured"]["text"]
+
+    # change this to the message that user submits on 
+    # your website using the 'event' variable
+    # msg_from_user = "Hello"
+    
     msg_from_user=''
     for message in event['messages']:
         msg_from_user += message['unstructured']['text'] + ' '
@@ -14,21 +21,31 @@ def lambda_handler(event, context):
     # Initiate conversation with Lex
     response = client.recognize_text(
             botId='XYWRSPCNFB', # MODIFY HERE
+            # botAliasId='MEBTCAUIGZ',  # cannot find this botAliasId
             botAliasId='TSTALIASID', # MODIFY HERE (new policy added)
             localeId='en_US',
             sessionId='test_session',
             text=msg_from_user)
     
     msg_from_lex = response.get('messages', [])
-    session_intent = response.get('interpretations',[])[0]['intent']['name']
-
+    session_intent = response.get('interpretations',[])[0]['intent']
+    
     if msg_from_lex:
+        
         response_from_lex=''
         for message in msg_from_lex:
             response_from_lex +=  message['content']+ ' '
             
         print(f"Message from Chatbot: {response_from_lex}")
-        print(f"Chatbot's sessionIntent: {session_intent}")
+        print(f"Chatbot's sessionIntent: {session_intent['name']}")
+
+        if session_intent['slots'] != None:
+            if session_intent['slots']['cuisine'] != None:
+                category = session_intent['slots']['cuisine']['value']['resolvedValues'][0]
+                print(category)
+            if session_intent['slots']['cuisine'] != None:
+                location = session_intent['slots']['location']['value']['resolvedValues'][0]
+                print(location)
         
         #ref https://docs.aws.amazon.com/lexv2/latest/APIReference/API_runtime_RecognizeText.html#API_runtime_RecognizeText_ResponseSyntax
         print(response)
