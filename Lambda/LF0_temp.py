a1599f2a35aff566ba64472bbc109d15329fd122
Tao707499 <120688956+Tao707499@users.noreply.github.com>
2023-02-23 21:04:23 -0500

    Create LF0_temp.py
    Fixed the connection with lex and front-end https://dining-concierge-6998.s3.amazonaws.com/test/chat.html
    Using botAliasId='TSTALIASID' with appropriate policy settings.

---
+++ Lambda/LF0_temp.py
@@ -0,0 +1,56 @@
+import boto3
+import json
+
+# Define the client to interact with Lex
+client = boto3.client('lexv2-runtime')
+
+def lambda_handler(event, context):
+
+    # msg_from_user = event['messages'][0]["unstructured"]["text"]
+
+    # change this to the message that user submits on 
+    # your website using the 'event' variable
+    # msg_from_user = "Hello"
+    
+    msg_from_user=''
+    for message in event['messages']:
+        msg_from_user += message['unstructured']['text'] + ' '
+
+    print(f"Message from frontend: {msg_from_user}")
+
+    # Initiate conversation with Lex
+    response = client.recognize_text(
+            botId='XYWRSPCNFB', # MODIFY HERE
+            # botAliasId='MEBTCAUIGZ',  # cannot find this botAliasId
+            botAliasId='TSTALIASID', # MODIFY HERE (new policy added)
+            localeId='en_US',
+            sessionId='test_session',
+            text=msg_from_user)
+    
+    msg_from_lex = response.get('messages', [])
+    if msg_from_lex:
+        
+        print(f"Message from Chatbot: {msg_from_lex[0]['content']}")
+        #ref https://docs.aws.amazon.com/lexv2/latest/APIReference/API_runtime_RecognizeText.html#API_runtime_RecognizeText_ResponseSyntax
+        print(response)
+
+        resp = {
+            'statusCode': 200,
+              "messages": [
+                  {
+                  "type": "unstructured",
+                  "unstructured": {
+                    "text": json.dumps(msg_from_lex[0]['content'])
+                    # ref: chat.js line 61-62
+                  }
+                }
+              ]
+        }
+
+        # modify resp to send back the next question Lex would ask from the user
+        
+        # format resp in a way that is understood by the frontend
+        # HINT: refer to function insertMessage() in chat.js that you uploaded
+        # to the S3 bucket
+
+        return resp
